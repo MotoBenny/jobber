40aaa1285daf7202cc871d917b9e5fa26446e98c
Benjamin <benj.james.carter@gmail.com>
2023-03-15 11:55:38 -0700

    borrowed code from another scrapper project

--- Jobber.py
+++ Jobber.py
@@ -1,29 +1,52 @@
 import requests
+# import urllib
 from bs4 import BeautifulSoup
+# import time
 
 
 """
 TODO: structure indeed URL with queries.
 """
 
-# https://www.indeed.com/q-junior-software-engineer-l-Seattle,-WA-jobs.html?vjk=8db6a695e03d6eb2
-
-# https://www.indeed.com/jobs?q=junior%20software%20engineer&l=Seattle%2C%20WA
-
-
-url = (
-    'https://www.indeed.com/jobs'
-    '?q=junior%20software%20engineer'
-    '&l=Seattle%2C%20WA')
+url = ("https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/"
+       "search?keywords=Software%2BEngineer&location=Seattle%2C%2BWashington"
+       "%2C%2BUnited%2BStates&locationId=&"
+       "geoId=104116203&sortBy=R&f_TPR=r604800&distance=50&start=")  # 0 or 25
 
 
-def get_data(url: str) -> str:
-    results = requests.get(url)
-    return results.text
+def linkedin_scrape(url, page_num):
+    next_page = url + str(page_num)  # constructing the URL
+    response = requests.get(str(next_page))  # getting content wuth requests
+    # parsing content with BS4d
+    soup = BeautifulSoup(response.content, 'html.parser')
 
+    jobs = soup.find_all('a', class_="base-card__full-link")
 
-def html_parse_with_soup(url: str):
-    html = get_data(url)
-    soup = BeautifulSoup(html, 'html.parser')
+    scraped_jobs_list = []
 
-    return (soup)
+    for listing in jobs:
+        job_url = listing.get('href')
+
+        response = requests.get(job_url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # if we have not already seen this job, scrape its data.
+        if job_url not in scraped_jobs_list:
+            # gets the job post body text
+            body = soup.find('div', class_="show-more-less-html__markup").text
+
+            # a is append, W will write a new file each time
+            with open('bulk_search_text.txt', 'a') as f:
+                f.write('\n')
+                f.write(str(job_url))  # writes the job URL
+                f.write('\n' + body + '\n')
+                # creates a human readable seperation between job postings
+
+            # adds the job_url to the scraped jobs list,
+            # after the scrape has been completed
+            scraped_jobs_list.append(job_url)
+            print(job_url)
+        else:
+            # if we hit this else, we have already scraped this job\
+            # and should proceed to the next in the for loop.
+            continue
